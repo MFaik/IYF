9d4c0c0c514c95127e0eca0ff275ceb0be471f2c
MFaik <m.faikekclr@gmail.com>
2022-02-12 00:46:21 +0300

    Expanding walls and camera

---
+++ Assets/Scripts/CameraExpand.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraExpand : MonoBehaviour
+{
+    [SerializeField] float CamGrowth = 0.1f;
+    [SerializeField] WallExpand wallExpand;
+    Camera m_camera;
+    int m_lastOrthographicSize;
+    void Start() {
+        m_camera = GetComponent<Camera>();
+        m_lastOrthographicSize = (int)m_camera.orthographicSize;
+        wallExpand.UpdateSize((m_lastOrthographicSize*2-2)*16f/9f,m_lastOrthographicSize*2-2,false);
+    }   
+
+    void Update() {
+        m_camera.orthographicSize += CamGrowth * Time.deltaTime;
+        if(m_lastOrthographicSize != (int)m_camera.orthographicSize){
+            m_lastOrthographicSize = (int)m_camera.orthographicSize;
+            wallExpand.UpdateSize((m_lastOrthographicSize*2-2)*16f/9f,m_lastOrthographicSize*2-2);
+        }
+    }
+}
---
+++ Assets/Scripts/WallExpand.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class WallExpand : MonoBehaviour
+{
+    [SerializeField] Transform[] Walls;
+    SpriteRenderer[] m_wallRenderers;
+    EdgeCollider2D m_edgeCollider;
+    
+    void Awake() {
+        m_edgeCollider = GetComponent<EdgeCollider2D>();
+        
+        if(Walls.Length != 4){
+            Debug.LogError("Wrong Wall Count");
+            return;
+        }
+
+        m_wallRenderers = new SpriteRenderer[4];
+        for(int i = 0;i < 4;i++){
+            m_wallRenderers[i] = Walls[i].GetComponent<SpriteRenderer>();
+        }
+    }
+
+    public void UpdateSize(float x,float y, bool animate = true){
+        float hx = x/2, hy = y/2;
+
+        if(animate){
+            Walls[0].DOMoveY(hy+.5f,.3f);
+            DOTween.To(() => m_wallRenderers[0].size, x => m_wallRenderers[0].size = x, new Vector2(x+2,1), .3f);
+
+            Walls[1].DOMoveY(-hy-.5f,.3f);
+            DOTween.To(() => m_wallRenderers[1].size, x => m_wallRenderers[1].size = x, new Vector2(x+2,1), .3f);
+
+            Walls[2].DOMoveX(hx+.5f,.3f);
+            DOTween.To(() => m_wallRenderers[2].size, x => m_wallRenderers[2].size = x, new Vector2(1,y), .3f);
+
+            Walls[3].DOMoveX(-hx-.5f,.3f);
+            DOTween.To(() => m_wallRenderers[3].size, x => m_wallRenderers[3].size = x, new Vector2(1,y), .3f);
+        } else {
+            Walls[0].position = new Vector2(0,hy+.5f);
+            m_wallRenderers[0].size = new Vector2(x+2,1);
+            Walls[1].position = new Vector2(0,-hy-.5f);
+            m_wallRenderers[1].size = new Vector2(x+2,1);
+            Walls[2].position = new Vector2(hx+.5f,0);
+            m_wallRenderers[2].size = new Vector2(1,y);
+            Walls[3].position = new Vector2(-hx-.5f,0);
+            m_wallRenderers[3].size = new Vector2(1,y);
+        }
+
+        m_edgeCollider.SetPoints(new List<Vector2>(){
+            new Vector2(hx,hy),
+            new Vector2(hx,-hy),
+            new Vector2(-hx,-hy),
+            new Vector2(-hx,hy),
+            new Vector2(hx,hy),
+        });
+    }
+
+}
