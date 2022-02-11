231f60b4e4f2af65f754a432b7636436500bb7f9
tixvage <mel10h3421@gmail.com>
2022-02-11 14:31:25 -0800

    faik adam bu hareket baya random oldu ve smoothluk katmamız gerek sabah kafa atarım ona

--- Assets/Scripts/RandomGezenAmogus.cs
+++ Assets/Scripts/RandomGezenAmogus.cs
@@ -5,12 +5,34 @@ using UnityEngine;
 public class RandomGezenAmogus : MonoBehaviour
 {
     [SerializeField] GameObject[] Walls;
-    
+    [SerializeField] float Speed;
+    [SerializeField] float StartWaitTime;
+    [SerializeField] Transform MoveStop;
+    [SerializeField] float MinX, MinY, MaxX, MaxY;
+
+    float m_waitTime;
+
     void Start() {
-        
+        m_waitTime = StartWaitTime;
+    
+        UpdateMoveStop();
     }
 
     void Update() {
-        
+        transform.position = Vector2.MoveTowards(transform.position, MoveStop.position, Speed * Time.deltaTime);
+
+        if(Vector2.Distance(transform.position, MoveStop.position) < 0.2f){
+            if(m_waitTime <= 0){
+                UpdateMoveStop();
+                m_waitTime = StartWaitTime;
+            } else{
+                m_waitTime -= Time.deltaTime;
+            }
+        }
+    }
+
+    void UpdateMoveStop() {
+        MoveStop.position = new Vector2(Random.Range(MinX, MaxX), Random.Range(MinY, MaxY));
+        Debug.Log(MoveStop.position);
     }
 }
