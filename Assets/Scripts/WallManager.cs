bf93eb3bf6fa86512261e37c281dd4f884dcd18c
tixvage <mel10h3421@gmail.com>
2022-02-12 10:03:25 -0800

    grid tarzı duvarlara sınır eklendi

--- Assets/Scripts/WallManager.cs
+++ Assets/Scripts/WallManager.cs
@@ -7,14 +7,18 @@ public class WallManager : MonoBehaviour
     Camera m_camera;
 
     [SerializeField] float GridSize;
-    
+
     [SerializeField] GameObject grid;
     [SerializeField] Transform wallParent;
+    [SerializeField] int MaxWallNumber;
+
     GameObject m_currentGrid;
     Wall m_currentGridWall;
+    int m_currentWallNumber = 0;
 
-    GameObject[,] grids;
-    Queue<Vector2> m_wallPositions = new Queue<Vector2>();
+    GameObject[,] m_allGrid;
+    public List<GameObject> m_currentWalls;
+    //Queue<Vector2> m_wallPositions = new Queue<Vector2>();
 
     public class Wall
     {
@@ -28,7 +32,7 @@ public class WallManager : MonoBehaviour
         public Wall(float x, float y, float gridSize) {
             X = x;
             Y = y;
-                  
+
             Collumn = (int)((5f - Y) / gridSize);
             Row = (int)((X + 8f) / gridSize);
 
@@ -38,61 +42,88 @@ public class WallManager : MonoBehaviour
         public Wall(Vector2 pos, float gridSize) {
             X = pos.x;
             Y = pos.y;
-                  
+
             Collumn = (int)((5f - Y) / gridSize);
             Row = (int)((X + 8f) / gridSize);
-        
+
             this.gridSize = gridSize;
         }
 
         public Vector2 GetRealPosition() {
             Vector2 position = new Vector2();
 
-            float startX = -7.5f;
-            float startY = 4.5f;
+            float startX = -8f + (gridSize / 2);
+            float startY = 5f - (gridSize / 2);
 
             position.x = startX + (this.Row * gridSize);
             position.y = startY - (this.Collumn * gridSize);
-            
+
             return position;
         }
     }
 
     void Start() {
         m_camera = Camera.main;
-        grids = new GameObject[99, 99];
+        m_allGrid = new GameObject[99, 99];
+        m_currentWalls = new List<GameObject>();
     }
 
     void Update() {
         if (Input.GetMouseButton(0)){
             SpawnWall(m_camera.ScreenToWorldPoint(Input.mousePosition));
         }
-    }
-
-    public void SpawnWall(Vector2 position) {
-        m_currentGridWall = new Wall(position, GridSize);
-
-        if (grids[m_currentGridWall.Row, m_currentGridWall.Collumn] == null){
-            m_currentGrid = Instantiate(grid, Vector3.zero, Quaternion.identity);
-            
-            m_currentGrid.transform.parent = wallParent;
-            grids[m_currentGridWall.Row, m_currentGridWall.Collumn] = m_currentGrid;
-
-            m_wallPositions.Enqueue(m_currentGridWall.GetRealPosition());
-            m_currentGrid.transform.position = m_currentGridWall.GetRealPosition();
-        } else {
-            Debug.Log("Dolu");
+        if (Input.GetMouseButtonUp(0)){
+            Reset();
         }
     }
 
-    public void RemoveWall(Vector2 position) {
+    void SpawnWall(Vector2 position) {
+        if (m_currentWalls.Count >= MaxWallNumber){
+            Destroy(m_currentWalls[0]);
+            m_currentWalls.RemoveAt(0);
+            
+            m_currentGridWall = new Wall(position, GridSize);
+            if (m_allGrid[m_currentGridWall.Row, m_currentGridWall.Collumn] == null){
+                m_currentGrid = Instantiate(grid, Vector3.zero, Quaternion.identity);
+                m_currentWalls.Add(m_currentGrid);
+
+                m_currentGrid.transform.parent = wallParent;
+                m_allGrid[m_currentGridWall.Row, m_currentGridWall.Collumn] = m_currentGrid;
+
+                m_currentGrid.transform.position = m_currentGridWall.GetRealPosition();
+            }
+        }
+        else{
+            m_currentGridWall = new Wall(position, GridSize);
+            if (m_allGrid[m_currentGridWall.Row, m_currentGridWall.Collumn] == null){
+                m_currentGrid = Instantiate(grid, Vector3.zero, Quaternion.identity);
+                m_currentWalls.Add(m_currentGrid);
+                Debug.Log(m_currentWalls[0]);
+                m_currentGrid.transform.parent = wallParent;
+                m_allGrid[m_currentGridWall.Row, m_currentGridWall.Collumn] = m_currentGrid;
+
+                m_currentGrid.transform.position = m_currentGridWall.GetRealPosition();
+            }
+        }
+    }
+
+    void RemoveWall(Vector2 position) {
         int collumn = (int)((5f - position.y) / GridSize);
         int row = (int)((position.x + 8f) / GridSize);
 
-        if (grids[row, collumn] != null){
-            Destroy(grids[row, collumn]);
-        } else{
-            Debug.Log("Zaten boş");
+        if (m_allGrid[row, collumn] != null){
+            Destroy(m_allGrid[row, collumn]);
         }
+    }
+
+    void RemoveWallIndex(int row, int collumn) {
+        if (m_allGrid[row, collumn] != null){
+            Destroy(m_allGrid[row, collumn]);
+        }
+    }
+
+    void Reset() {
+        m_currentWallNumber = 0;
+        m_currentWalls.Clear();
     }
 }
