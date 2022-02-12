a1dad35ddac2a7f5c18814a0534e89fcc488b342
tixvage <mel10h3421@gmail.com>
2022-02-12 07:36:37 -0800

    eren artık al başımdan şu wallları

--- Assets/Scripts/DrawWall.cs
+++ Assets/Scripts/DrawWall.cs
@@ -8,6 +8,7 @@ public class DrawWall : MonoBehaviour
     [SerializeField] int MaxSize;
     [SerializeField] float MaxPullSize;
     [SerializeField] Transform WallPool;
+    [SerializeField] GameObject BoxColliderPrefab;
     
     GameObject m_currentWall;
 
@@ -15,6 +16,8 @@ public class DrawWall : MonoBehaviour
     EdgeCollider2D m_edgeCollider;
     List<Vector2> m_mousePositions;
     Camera m_camera;
+    Vector2 m_colliderCounter = Vector2.zero;
+    Vector2 m_colliderEnd = Vector2.zero;
 
     void Start() {
         m_camera = Camera.main;
@@ -37,7 +40,7 @@ public class DrawWall : MonoBehaviour
         m_currentWall = Instantiate(WallPrefab, Vector3.zero, Quaternion.identity);
 
         if(WallPool)
-            m_currentWall.GetComponent<Transform>().SetParent(WallPool);
+            m_currentWall.transform.SetParent(WallPool);
 
         m_lineRenderer = m_currentWall.GetComponent<LineRenderer>();
         m_edgeCollider = m_currentWall.GetComponent<EdgeCollider2D>();
@@ -46,17 +49,40 @@ public class DrawWall : MonoBehaviour
         m_mousePositions.Add(m_camera.ScreenToWorldPoint(Input.mousePosition));
         m_mousePositions.Add(m_camera.ScreenToWorldPoint(Input.mousePosition));
 
+        m_colliderEnd = m_camera.ScreenToWorldPoint(Input.mousePosition);
+        m_colliderCounter = Vector2.zero;
+
         m_lineRenderer.SetPosition(0, m_mousePositions[0]);
         m_lineRenderer.SetPosition(1, m_mousePositions[1]);
 
         m_edgeCollider.points = m_mousePositions.ToArray();
     }
 
+    void SpawnColliders() {
+        GameObject newCollider = Instantiate(BoxColliderPrefab, Vector3.zero, Quaternion.identity);
+        newCollider.transform.SetParent(m_currentWall.transform);
+        BoxCollider2D collider = newCollider.GetComponent<BoxCollider2D>();
+        newCollider.transform.position = m_colliderEnd;
+        collider.size = new Vector2(1f, 1f);
+    }
+
     void UpdateWall(Vector2 newMousePos) {
         Vector2 fromLast = newMousePos - m_mousePositions[m_mousePositions.Count - 1];
+        
+        m_colliderEnd += fromLast;
+
+        fromLast.x = Mathf.Abs(fromLast.x);
+        fromLast.y = Mathf.Abs(fromLast.y);
+        
+        if (m_colliderCounter.x > 0.5 || m_colliderCounter.y > 0.5){
+            SpawnColliders();
+            m_colliderCounter = Vector2.zero;
+        } else {
+            m_colliderCounter += fromLast;
+        }
 
         //cok hizli cekiyorsak line gg
-        if (!(fromLast.x > MaxPullSize || fromLast.x < -MaxPullSize || fromLast.y > MaxPullSize || fromLast.y < -MaxPullSize)){
+        if (!(fromLast.x > MaxPullSize || fromLast.y > MaxPullSize)){
             m_mousePositions.Add(newMousePos);
             m_lineRenderer.positionCount++;
             m_lineRenderer.SetPosition(m_lineRenderer.positionCount - 1, newMousePos);
