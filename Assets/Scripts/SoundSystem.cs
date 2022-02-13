fbeb6ac4cfa2f5d2705fb8467238ae858323a8c4
MFaik <m.faikekclr@gmail.com>
2022-02-13 18:14:15 +0300

    mute icon + end animation

--- Assets/Scripts/SoundSystem.cs
+++ Assets/Scripts/SoundSystem.cs
@@ -1,20 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Audio;
 
 public class SoundSystem : MonoBehaviour
 {
+    [SerializeField] Sprite PlaySprite;
+    [SerializeField] Sprite MutedSprite;
+
+    Image m_image;
+
     public AudioMixer m_audioMixer;
 
     bool muted = false;
 
+    void Start() {
+        m_image = GetComponent<Image>();
+    }
+
     public void SetVolume() {
         if (muted){
+            m_image.sprite = PlaySprite;
             muted = false;
             m_audioMixer.SetFloat("Volume", 0f);
         }
         else{
+            m_image.sprite = MutedSprite;
             muted = true;
             m_audioMixer.SetFloat("Volume", -80f);
         }
